3e51aa55dfda56cc84f8312660a1d55c1c7b0d99
Juan Lopez <110072243+22001223@users.noreply.github.com>
2024-01-31 21:29:17 -0600

    Inicializacion del reto

---
+++ src/script.js
@@ -0,0 +1,35 @@
+// EJERCICIO 1
+console.log('funciona');
+function calcularExponente() {
+    let base      = parseInt(document.getElementById('base'));
+    let exponente = parseInt(document.getElementById('exponente'));
+    let resultado1 = Math.pow(base, exponente);
+    console.log(`base: ${base} exponente: ${exponente}`);
+}
+
+
+
+
+// EJERCICIO 2
+
+
+
+// EJERCICIO 3
+
+
+
+// EJERCICIO 4
+
+
+
+// EJERCICIO 5
+
+
+
+// EJERCICIO 6
+
+
+
+// EJERCICIO 7
+
+
